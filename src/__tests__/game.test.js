4b21c4aff2bf92044b2fb9e0c8205885b37dd170
Ole Kristian Sunde <ole.sunde@gmail.com>
2024-06-29 22:21:07 +0200

    refactor towards functional

--- src/__tests__/game.test.js
+++ src/__tests__/game.test.js
@@ -1,17 +1,21 @@
 import { Game } from '../game'
 
 test('gutter game', () => {
-  const game = new Game()
-  for (let i = 0; i < 20; i++) {
-    game.roll(0)
-  }
-  expect(game.score()).toBe(0)
+  const rolls = rollMany(20, 0)
+  const game = rolls.reduce(applyRoll, new Game())
+  expect(game.score).toBe(0)
 })
 
 test('all ones', () => {
-  const game = new Game()
-  for (let i = 0; i < 20; i++) {
-    game.roll(1)
-  }
-  expect(game.score()).toBe(20)
+  const rolls = rollMany(20, 1)
+  const game = rolls.reduce(applyRoll, new Game())
+  expect(game.score).toBe(20)
 })
+
+function applyRoll(game, pins) {
+  return game.roll(pins)
+}
+
+function rollMany(n, pins) {
+  return Array(n).fill(pins)
+}
--- src/game.js
+++ src/game.js
@@ -1,13 +1,33 @@
 export class Game {
-  constructor() {
-    this._score = 0
+  constructor(rolls = Array(21).fill(0), currentRoll = 0) {
+    this._rolls = rolls
+    this._currentRoll = currentRoll
   }
 
-  score() {
-    return this._score
+  get score() {
+    function scoreAccumulator(rolls) {
+      return ({ score, frameIndex }) => {
+        function scoreFrorFrame() {
+          return rolls[frameIndex] + rolls[frameIndex + 1]
+        }
+
+        return {
+          score: score + scoreFrorFrame(),
+          frameIndex: frameIndex + 2,
+        }
+      }
+    }
+
+    const { score } = Array(10)
+      .fill(0)
+      .reduce(scoreAccumulator(this._rolls), { score: 0, frameIndex: 0 })
+
+    return score
   }
 
   roll(pins) {
-    this._score += pins
+    const rollsCopy = [...this._rolls]
+    rollsCopy[this._currentRoll] = pins
+    return new Game(rollsCopy, this._currentRoll + 1)
   }
 }
